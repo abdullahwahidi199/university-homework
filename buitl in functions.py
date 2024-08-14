8ae337f36bc2198f5a85bc59c30cc787c4ba6104
abdullahwahidi199 <wahidia53@gmail.com>
2024-08-14 09:35:01 +0430

    Add files via upload

---
+++ buitl in functions.py
@@ -0,0 +1,48 @@
+getattr: Returns the value of the named attribute.
+globals: Returns a dictionary representing the current global symbol table.
+hasattr: Returns True if the object has the named attribute.
+hash: Returns the hash value of the object.
+help: Invokes the built-in help system.
+hex: Converts an integer to a hexadecimal string.
+id: Returns the identity of an object.
+input: Reads a line from input.
+int: Converts a number or string to an integer.
+isinstance: Returns True if the object is an instance of a class or a tuple of classes.
+issubclass: Returns True if class is a subclass of classinfo.
+iter: Returns an iterator object.
+len: Returns the length of an object.
+list: Creates a new list.
+locals: Updates and returns a dictionary representing the current local symbol table.
+map: Applies function to every item of iterable and returns a list of the results.
+max: Returns the largest item in an iterable or the largest of two or more arguments.
+memoryview: Returns a memory view object.
+min: Returns the smallest item in an iterable or the smallest of two or more arguments.
+next: Retrieves the next item from an iterator.
+object: Returns a new featureless object.
+oct: Converts an integer to an octal string.
+open: Opens a file.
+ord: Returns the Unicode code point for a single character.
+pow: Returns x to the power of y; if z is present, returns x to the power of y, modulo z.
+print: Prints objects to the text stream file.
+abs: Returns the absolute value of a number.
+all: Returns True if all elements of the iterable are true.
+any: Returns True if any element of the iterable is true.
+ascii: Returns a string containing a printable representation of an object.
+bin: Converts an integer to a binary string.
+bool: Converts a value to a Boolean, using the standard truth testing procedure.
+bytearray: Returns a new array of bytes.
+bytes: Returns a new "bytes" object.
+callable: Returns True if the object appears callable.
+classmethod: Transforms a method into a class method.
+compile: Compiles source into a code or AST object.
+complex: Creates a complex number.
+delattr: Deletes the named attribute from the object.
+dict: Creates a new dictionary.
+dir: Tries to return a list of valid attributes of the object.
+divmod: Returns a tuple of the quotient and remainder of dividing two numbers.
+enumerate: Returns an enumerate object.
+eval: Evaluates a given expression.
+exec: Executes the given object.
+filter: Constructs an iterator from elements of iterable for which function returns true.
+float: Converts a number or string to a floating point number.
+
